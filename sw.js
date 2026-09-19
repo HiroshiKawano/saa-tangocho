8ac4bd71a7e69addb8b92f4bb86a9ac892e7314f
stamppK <kawano@stampp.co.jp>
2026-09-20 01:38:24 +0900

    feat: キーワードと正解のサービス名を約 2 倍に拡大（長い文字列は一段小さく）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-2a3196b77ebb';
+const CACHE = 'saa-tangocho-dc858fe386cc';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
