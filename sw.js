c7b925255124716b73ac5d027c2830c18205888d
stamppK <kawano@stampp.co.jp>
2026-09-20 13:07:21 +0900

    feat: 0 章 罠のパターンの表を問題・正解・解説の形式に書き換え

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-bcb9b34b3678';
+const CACHE = 'saa-tangocho-c66c99ef65ed';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
