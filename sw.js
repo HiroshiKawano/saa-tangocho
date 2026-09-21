c51fad63169ec61c322e3f31643e1c3ebed4ad47
stamppK <kawano@stampp.co.jp>
2026-09-21 23:03:11 +0900

    feat: 試験日 9/28 に合わせて残りを 9/22・23・26 に均等配分し、総復習を 9/27 に

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-8c6f3000a78c';
+const CACHE = 'saa-tangocho-5287db8768d7';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
