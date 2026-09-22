ddd45bfd399b30e245b8a42b89a3fea29d561a27
stamppK <kawano@stampp.co.jp>
2026-09-22 19:56:48 +0900

    feat: RTO/RPO 表のフェイルオーバーのキーワードに説明を追記

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-8abd991b06d8';
+const CACHE = 'saa-tangocho-1cf48995ecc7';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
