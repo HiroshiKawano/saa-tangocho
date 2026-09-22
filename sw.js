8e39b601f0d597380ecad99b710bc8edeb9c40b0
stamppK <kawano@stampp.co.jp>
2026-09-22 13:34:03 +0900

    feat: コスト管理表の Cost Allocation Tags の正解に日本語名を追記

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-d317fd4b16ce';
+const CACHE = 'saa-tangocho-af88676dd102';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
