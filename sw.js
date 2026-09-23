d2df0739d0519b21fa268eea5bde3ee6da1be2dd
stamppK <kawano@stampp.co.jp>
2026-09-23 16:45:31 +0900

    feat: Route 53 表のレイテンシールーティングのキーワードを短縮

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-a92580a2877e';
+const CACHE = 'saa-tangocho-259dae7eaab7';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
