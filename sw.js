9c2ca412ef08b9af1a251c264032d0a2f89ede2c
stamppK <kawano@stampp.co.jp>
2026-09-23 14:02:32 +0900

    feat: Auto Scaling 表の Lifecycle Hook のキーワードに補足を追記

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-5969424751dc';
+const CACHE = 'saa-tangocho-19a2a26dfe25';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
