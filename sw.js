28df67ac9b1fe6853981bb8653cadc27a8f95198
stamppK <kawano@stampp.co.jp>
2026-09-23 15:54:55 +0900

    feat: Route 53 表のフェイルオーバールーティングの正解に「経路制御」を追記

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-c6b71ab19728';
+const CACHE = 'saa-tangocho-a92580a2877e';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
