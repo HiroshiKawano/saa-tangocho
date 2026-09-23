fdcc30aa436a5cee67be640eeeca24661eb60b70
stamppK <kawano@stampp.co.jp>
2026-09-23 18:10:59 +0900

    feat: エッジ配信表の CloudFront Functions のキーワードに補足を追記

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-a90d0b77d964';
+const CACHE = 'saa-tangocho-1f80779b4312';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
