9c88266d22000a6316967440b2ea5f495834f0be
stamppK <kawano@stampp.co.jp>
2026-09-23 12:44:21 +0900

    feat: EC2 購入表の HPC の行に日本語の補足を追記

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-2f839372fba1';
+const CACHE = 'saa-tangocho-5969424751dc';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
