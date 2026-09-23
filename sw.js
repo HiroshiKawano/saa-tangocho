917cc2093468c477c579bb729024c1a1b923cdf2
stamppK <kawano@stampp.co.jp>
2026-09-23 15:44:38 +0900

    feat: Route 53 表の Zone Apex のキーワードに補足を追記

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュ名にページ内容のハッシュを含め、更新時に古いキャッシュを捨てる
-const CACHE = 'saa-tangocho-75c20fed0bec';
+const CACHE = 'saa-tangocho-c6b71ab19728';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
